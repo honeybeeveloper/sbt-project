848bc33b8f923e831c3c3af88c05ec47d242d3fd
honeybeeveloper <honeybeeveloper@naver.com>
2025-07-08 16:41:13 +0900

    tasks for specific account

--- src/sbt_project/__init__.py
+++ src/sbt_project/__init__.py
@@ -1,6 +1,8 @@
+import os
 from configs.config import Config
 from configs.logging import get_logger
 
 app_config = Config(config_file='env.yaml')
 app_logger = get_logger('sbt-project', app_config)
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
--- src/sbt_project/common/utils.py
+++ src/sbt_project/common/utils.py
@@ -1,5 +1,14 @@
 import os
+import json
+
+from sbt_project.configs.config import Config
 
 def create_dir(dir_path):
     if not os.path.isdir(dir_path):
         os.makedirs(dir_path)
+
+def json_to_dict(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data
