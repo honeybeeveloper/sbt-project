3b40e94c0c6a7a774c8a1a36c13c56e84adf7229
honeybeeveloper <honeybeeveloper@naver.com>
2025-07-31 16:00:35 +0900

    python 실행 위치 기준 디렉토리에 "src" 포함여부에 따라 config 파일 읽어오는 경로 할당

--- src/sbt_project/__init__.py
+++ src/sbt_project/__init__.py
@@ -7,8 +7,5 @@ from sbt_project.configs.logging import get_logger
 app_config = Config(config_file='env.yaml')
 app_logger = get_logger('sbt-project', app_config)
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-json_path = os.path.join(ROOT_DIR, 'converted_data.json')
-# json_path = os.path.join(ROOT_DIR,'src','sbt_project' , 'converted_data.json') // out-main.py
+json_path = os.path.join(app_config.env_dir, 'converted_data.json')
 sales_activity = utils.json_to_dict(json_path)
